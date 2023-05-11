ca3af414cab7fbbcd59a2ac2359c5d938a58d64f
baldevkumar@logiceasetecno.com <devbinogiv3@gmail.com>
2023-05-11 09:58:13 +0530

    translation 3

--- i18n.tsx
+++ i18n.tsx
@@ -1,6 +1,6 @@
 import i18next from "i18next";
-import en from "../soundhunters_RN/strings/en.json"
-import german from "../soundhunters_RN/strings/german.json";
+import en from "../soundhuntersRN/strings/en.json"
+import german from "../soundhuntersRN/strings/german.json";
 import { initReactI18next } from "react-i18next";
 i18next.use(initReactI18next).init({
   compatibilityJSON: "v3",
